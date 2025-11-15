024c32bdd430034d78cf4af1b67dcea76ce96695
Tushar Konala <tusharkonala284@gmail.com>
2025-11-15 12:58:25 +0530

    Refactor getCourses function to retrieve and display published courses by author

--- index.js
+++ index.js
@@ -27,4 +27,15 @@ async function createCourse() {
   console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+  const result = await Course.find({
+    author: "Mosh",
+    isPublished: true,
+  })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(result);
+}
+
+getCourses();
